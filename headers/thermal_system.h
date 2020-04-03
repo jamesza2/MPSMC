6bd34e64d6aa2ca2f55f8a010316c281e5503847
James_Allen <jallen37@stanford.edu>
2020-04-03 11:15:23 -0500

    cerr spit for repeats vector

--- headers/thermal_system.h
+++ headers/thermal_system.h
@@ -167,6 +167,11 @@ class ThermalSystem{
 						accept = true;
 					}
 				}
+				std::cerr << "Repeats vector: ";
+				for(int rep : repeats){
+					std::cerr << rep << " ";
+				}
+				std::cerr << endl;
 				if(step_number >= num_steps){
 					//sample estimated error
 					double estimation_at_old_position = repeats[0]*norm_of_original_wavefunction/(singular_values[0]*std::sqrt(old_norm_squared));
